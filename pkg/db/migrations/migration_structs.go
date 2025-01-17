52750ff5cb82d278c10d3845c3c0b8abe3fcc343
Wei Liu <liuweixa@redhat.com>
2025-01-17 01:04:33 -0500

    test migration
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- pkg/db/migrations/migration_structs.go
+++ pkg/db/migrations/migration_structs.go
@@ -1,13 +1,18 @@
 package migrations
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"time"
 
 	"gorm.io/gorm"
 
 	"github.com/go-gormigrate/gormigrate/v2"
+	"github.com/openshift-online/maestro/pkg/logger"
 )
+
+var log = logger.NewOCMLogger(context.Background())
 
 // gormigrate is a wrapper for gorm's migration functions that adds schema versioning and rollback capabilities.
 // For help writing migration steps, see the gorm documentation on migrations: http://doc.gorm.io/database.html#migration
@@ -38,6 +43,23 @@ var MigrationList = []*gormigrate.Migration{
 	alterEventInstances(),
 }
 
+// CleanUpDirtyData clean up the dirty data before migrating the tables.
+// When we add new constraints to old tables, we should especially consider the possibility of dirty data.
+func CleanUpDirtyData(db *gorm.DB) error {
+	// Clean up the dirty data from event_instances tale.
+	// the new constraints are added by migration `202412181141_alter_event_instances`,
+	// before do this migration, we should check and clean up dirty data
+	if err := cleanupUnreferencedData(db, "event_instances", "server_instances", "instance_id", "id"); err != nil {
+		return err
+	}
+
+	if err := cleanupUnreferencedData(db, "event_instances", "status_events", "event_id", "id"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // Model represents the base model struct. All entities will have this struct embedded.
 type Model struct {
 	ID        string `gorm:"primary_key"`
@@ -66,6 +88,40 @@ func CreateFK(g2 *gorm.DB, fks ...fkMigration) error {
 			return err
 		}
 	}
+	return nil
+}
+
+func cleanupUnreferencedData(db *gorm.DB, table, referencedTable, fk, pk string) error {
+	if !db.Migrator().HasTable(table) {
+		return nil
+	}
+
+	var nullIDs []sql.NullString
+	query := fmt.Sprintf(`
+        SELECT %s.%s
+        FROM %s
+        LEFT JOIN %s ON %s.%s = %s.%s
+        WHERE %s.%s IS NULL
+    `, table, fk, table, referencedTable, table, fk, referencedTable, pk, referencedTable, pk)
+
+	if err := db.Raw(query).Pluck(fk, &nullIDs).Error; err != nil {
+		return fmt.Errorf("failed to run query %s: %v", query, err)
+	}
+
+	var invalidIDs []string
+	for _, nullID := range nullIDs {
+		if nullID.Valid {
+			invalidIDs = append(invalidIDs, nullID.String)
+		}
+	}
+
+	if len(invalidIDs) != 0 {
+		log.Infof("clean up the dirty data from %s (fk=%s): %v", table, fk, invalidIDs)
+		if err := db.Table(table).Where(fk+" IN ?", invalidIDs).Delete(nil).Error; err != nil {
+			return fmt.Errorf("failed to delete dirty data: %v", err)
+		}
+	}
+
 	return nil
 }
 
